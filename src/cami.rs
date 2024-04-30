3543a7b1cc860b36ba9b22d3736bcdd7205cf82d
Peter Kehl <peter.kehl@gmail.com>
2024-04-30 16:27:40 -0700

    Cami: pub T is deprecated (until further notice)

--- src/cami.rs
+++ src/cami.rs
@@ -5,14 +5,14 @@ use core::fmt::{self, Debug};
 use core::ops::DerefPure;
 use core::ops::{Deref, DerefMut};
 
-// @TODO once agreed & futureproofed, make the `T` field `pub`.
+// @TODO once agreed & futureproofed, remove `#[deprecated...]` on field `pub T`.
 //
 // Having an `Rhs` generic (for `CamiPartialEq`) would need a phantom data field, so we couldn't
 // easily pattern match this etc.
 //
 // pub struct Cami<T: CamiPartialEq<Rhs>, Rhs: ?Sized = Self>(pub T);
 #[repr(transparent)]
-pub struct Cami<T: CamiPartialEq + ?Sized>(T);
+pub struct Cami<T: CamiPartialEq + ?Sized>(#[deprecated = "unstable"] pub T);
 //----------
 
 impl<T: CamiPartialEq> Cami<T> {
@@ -21,12 +21,14 @@ impl<T: CamiPartialEq> Cami<T> {
     }
 
     pub fn in_cami(&self) -> &T {
+        #[allow(deprecated)]
         &self.0
     }
     /// Consume [self], return the wrapped data. We COULD just use `self.0` (or
     /// `variable_holding_the_instance.0`) - but, then it can't be easily searched for in source
     /// code.
     pub fn from_cami(self) -> T {
+        #[allow(deprecated)]
         self.0
     }
 }
@@ -36,6 +38,7 @@ impl<T: CamiPartialEq + Copy> Cami<T> {
     /// `variable_holding_the_instance.0`) - but, then it can't be easily searched for in source
     /// code.
     pub fn from_cami_copy(&self) -> T {
+        #[allow(deprecated)]
         self.0
     }
 }
@@ -45,6 +48,7 @@ impl<T: CamiPartialEq + Clone> Cami<T> {
     /// `self.0.clone()` (or `variable_holding_the_instance.0.clone()`) - but, then it can't be
     /// easily searched for in source code.
     pub fn from_cami_clone(&self) -> T {
+        #[allow(deprecated)]
         self.0.clone()
     }
 }
@@ -117,9 +121,13 @@ impl<T: CamiPartialEq> IntoCamiSlice for [T] {
 
 impl<T: Clone + CamiPartialEq> Clone for Cami<T> {
     fn clone(&self) -> Self {
-        Self(self.0.clone())
+        Self(
+            #[allow(deprecated)]
+            self.0.clone(),
+        )
     }
     fn clone_from(&mut self, source: &Self) {
+        #![allow(deprecated)]
         self.0 = source.0.clone();
     }
 }
@@ -128,13 +136,20 @@ impl<T: Copy + CamiPartialEq> Copy for Cami<T> {}
 
 impl<T: Debug + CamiPartialEq> Debug for Cami<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Cami").field("0", &self.0).finish()
+        f.debug_struct("Cami")
+            .field(
+                "0",
+                #[allow(deprecated)]
+                &self.0,
+            )
+            .finish()
     }
 }
 
 impl<T: CamiPartialEq> PartialEq for Cami<T> {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.eq_full(&other.0)
     }
 }
@@ -145,12 +160,14 @@ impl<T: Eq + CamiPartialEq> Eq for Cami<T> {}
 impl<T: CamiPartialEq + ?Sized> Deref for Cami<T> {
     type Target = T;
     fn deref(&self) -> &T {
+        #[allow(deprecated)]
         &self.0
     }
 }
 
 impl<T: CamiPartialEq + ?Sized> DerefMut for Cami<T> {
     fn deref_mut(&mut self) -> &mut T {
+        #[allow(deprecated)]
         &mut self.0
     }
 }
@@ -165,11 +182,13 @@ impl<T: CamiPartialEq> CamiPartialEq for Cami<T> {
     const LOCALITY: Locality = T::LOCALITY;
     #[inline]
     fn eq_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.eq_local(&other.0)
     }
 
     #[inline]
     fn eq_non_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         !self.0.eq_non_local(&other.0)
     }
 }
@@ -178,41 +197,51 @@ impl<T: CamiPartialEq> CamiPartialEq for Cami<T> {
 // more level of `Cami`, but it's possible.
 impl<T: CamiPartialOrd> CamiPartialOrd for Cami<T> {
     fn partial_cmp_local(&self, other: &Self) -> Option<Ordering> {
+        #![allow(deprecated)]
         self.0.partial_cmp_local(&other.0)
     }
     fn partial_cmp_non_local(&self, other: &Self) -> Option<Ordering> {
+        #![allow(deprecated)]
         self.0.partial_cmp_non_local(&other.0)
     }
     #[inline]
     fn lt_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.lt_local(&other.0)
     }
     #[inline]
     fn lt_non_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.lt_non_local(&other.0)
     }
     #[inline]
     fn le_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.le_local(&other.0)
     }
     #[inline]
     fn le_non_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.le_non_local(&other.0)
     }
     #[inline]
     fn gt_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.gt_local(&other.0)
     }
     #[inline]
     fn gt_non_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.gt_non_local(&other.0)
     }
     #[inline]
     fn ge_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.ge_local(&other.0)
     }
     #[inline]
     fn ge_non_local(&self, other: &Self) -> bool {
+        #![allow(deprecated)]
         self.0.ge_non_local(&other.0)
     }
 }
@@ -222,11 +251,13 @@ impl<T: CamiPartialOrd> CamiPartialOrd for Cami<T> {
 impl<T: Ord + PartialOrd + PartialEq + CamiPartialEq + CamiOrd> CamiOrd for Cami<T> {
     #[inline]
     fn cmp_local(&self, other: &Self) -> Ordering {
+        #![allow(deprecated)]
         self.0.cmp_local(&other.0)
     }
 
     #[inline]
     fn cmp_non_local(&self, other: &Self) -> Ordering {
+        #![allow(deprecated)]
         self.0.cmp_non_local(&other.0)
     }
 }
